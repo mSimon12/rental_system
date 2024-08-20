f46146164782a049f5b816a55ce612fc415b1f64
mSimon12 <marcelo_simon@hotmail.com>
2024-08-20 19:41:50 +0200

    feat: initialize app via flask

---
+++ flaskr/__init__.py
@@ -0,0 +1,39 @@
+from flask import Flask
+import os
+
+from . import db
+from . import store
+from . import shelf_manager
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/")
+    def main_page():
+        return "Welcome!"
+
+    db.init_app(app)
+
+    app.register_blueprint(shelf_manager.bp)
+    app.register_blueprint(store.bp)
+
+    return app
