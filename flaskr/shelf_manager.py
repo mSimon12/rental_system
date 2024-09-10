31d401b4ae1a38a8129315b3726b357321c3afbd
mSimon12 <marcelo_simon@hotmail.com>
2024-09-10 22:56:03 +0200

    feat: add manager webpage

--- flaskr/shelf_manager.py
+++ flaskr/shelf_manager.py
@@ -1,7 +1,10 @@
 import logging
 
-from flask import Blueprint, current_app, request, jsonify, flash
+from flask import Blueprint, current_app, request, jsonify, flash, render_template, url_for, redirect
 from logging import Logger
+from flaskr.forms import AddItemForm
+import requests as req
+import json
 
 from flaskr.db import get_db
 
@@ -11,117 +14,27 @@ logger.addHandler(file_handler)
 
 bp = Blueprint('manager', __name__, url_prefix='/manager')
 
+@bp.route("/", methods=['GET', 'POST'])
+def manager():
+    add_item_form = AddItemForm()
 
-def validate_input(req_input, pattern):
-    for req_key in req_input:
-        try:
-            assert req_key in pattern.keys()
-            assert isinstance(req_input[req_key], pattern[req_key])
-        except AssertionError:
-            return False
+    if request.method == 'POST':
+        api_request = {
+            "item": add_item_form.name.data,
+            "description": add_item_form.description.data,
+            "stock": add_item_form.stock.data
+        }
 
-    return True
+        resp = req.post('http://127.0.0.1:5000/api/items', json= api_request)
 
-
-item_pattern = {
-    'item': str,
-    'description': str,
-    'stock': int
-}
-
-
-def get_item_info_from_db(item_name):
-    db = get_db()
-
-    item = db.execute(
-        'SELECT * FROM generic_shelf WHERE item = ?', (item_name,)
-    ).fetchone()
-
-    if item:
-        item = dict(item)
-        item.pop('id')
-
-    return item
-
-
-@bp.route('/item')
-def get_item_request():
-    request_input = request.get_json()
-
-    if 'item' not in request_input.keys():
-        return 'Item name missing!', 400
-
-    info = get_item_info_from_db(request_input['item'])
-
-    if info:
-        return jsonify(info)
+    resp = req.get('http://127.0.0.1:5000/api/items')
+    if resp.status_code == 200:
+        items = resp.json()
     else:
-        return 'Required item not found', 404
+        return resp
 
 
-def add_item_to_db(item_info):
-    db = get_db()
+    return render_template("manager.html",
+                           template_items = items,
+                           template_add_item = add_item_form)
 
-    try:
-        db.execute(
-            "INSERT INTO generic_shelf (item, description, stock_size, available) VALUES (?, ?, ?, ?)",
-            (item_info['item'], item_info['description'], item_info['stock'], item_info['stock']),
-        )
-        db.commit()
-    except db.IntegrityError:
-        return f"Item {item_info['item']} is already registered."
-    else:
-        return ''
-
-
-@bp.route('/item', methods=['POST'])
-def add_item_request():
-
-    request_input = request.get_json()
-    valid = validate_input(request_input, item_pattern)
-
-    # Check it is logged
-    if valid:
-        error = add_item_to_db(request_input)
-
-        if not error:
-            return '', 201
-        else:
-            return error, 400
-    else:
-        return '', 400
-
-
-def delete_item_from_db(item_name):
-    db = get_db()
-
-    item = db.execute(
-        'SELECT * FROM generic_shelf WHERE item = ?', (item_name,)
-    ).fetchone()
-
-    if item:
-        try:
-            db.execute(
-                "DELETE FROM generic_shelf WHERE item = ?",
-                (item_name,),
-            )
-            db.commit()
-        except db.Error:
-            return False
-        else:
-            return True
-
-    return False
-
-
-@bp.route('/item', methods=['DELETE'])
-def delete_item_request():
-    request_input = request.get_json()
-
-    if 'item' not in request_input.keys():
-        return 'Item name missing!', 400
-
-    if delete_item_from_db(request_input['item']):
-        return '', 204
-    else:
-        return 'Required item not found', 404
--- flaskr/store_forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
-from wtforms.validators import DataRequired
-
-class CommentForm(FlaskForm):
-    new_comment = TextAreaField("New Comment:", validators=[DataRequired()])
-    submit_comment = SubmitField("Submit")
