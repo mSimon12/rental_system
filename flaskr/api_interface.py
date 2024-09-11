142fc07030d0fa4ef61400676d6e6d681e369fb7
mSimon12 <marcelo12.simon@gmail.com>
2024-09-11 21:51:36 +0200

    refactor: decoupled store and manager pages from api by adding api_interface

---
+++ flaskr/api_interface.py
@@ -0,0 +1,37 @@
+import requests
+
+def get_item_info(item_id):
+    resp = requests.get(f"http://127.0.0.1:5000/api/items/{item_id}")
+    if resp.status_code == 200:
+        return dict(resp.json())
+    else:
+        return None
+
+
+def get_items_id_map():
+    resp = requests.get('http://127.0.0.1:5000/api/items')
+    if resp.status_code == 200:
+        return dict(resp.json())
+    else:
+        return None
+
+
+def get_store_items():
+    store_items = {}
+
+    items_map = get_items_id_map()
+    if items_map:
+        for key in items_map.keys():
+            item_info = get_item_info(key)
+            if item_info:
+                store_items[key] = item_info
+
+    return store_items
+
+
+def add_new_item_to_store(new_item):
+    resp = requests.post('http://127.0.0.1:5000/api/items', json= new_item)
+    if resp.status_code == 201:
+        return True
+
+    return False
--- flaskr/shelf_manager.py
+++ flaskr/shelf_manager.py
@@ -3,10 +3,8 @@ import logging
 from flask import Blueprint, current_app, request, jsonify, flash, render_template, url_for, redirect
 from logging import Logger
 from flaskr.forms import AddItemForm
-import requests as req
-import json
+from flaskr.api_interface import get_store_items, add_new_item_to_store
 
-from flaskr.db import get_db
 
 logger = Logger('manager', level='DEBUG')
 file_handler = logging.FileHandler("debug.log")
@@ -14,25 +12,25 @@ logger.addHandler(file_handler)
 
 bp = Blueprint('manager', __name__, url_prefix='/manager')
 
+
 @bp.route("/", methods=['GET', 'POST'])
 def manager():
-    add_item_form = AddItemForm()
+    add_item_form = AddItemForm(request.form)
 
-    if request.method == 'POST':
+    if add_item_form.validate_on_submit():
         api_request = {
             "item": add_item_form.name.data,
             "description": add_item_form.description.data,
             "stock": add_item_form.stock.data
         }
 
-        resp = req.post('http://127.0.0.1:5000/api/items', json= api_request)
+        call_status = add_new_item_to_store(api_request)
+        if not call_status:
+            print("Not able to add new item!")
+        else:
+            return redirect(url_for('manager.manager'))
 
-    resp = req.get('http://127.0.0.1:5000/api/items')
-    if resp.status_code == 200:
-        items = resp.json()
-    else:
-        return resp
-
+    items = get_store_items()
 
     return render_template("manager.html",
                            template_items = items,
