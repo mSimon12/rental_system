df932f2c2fdca46c16b1917b9a41e2b257a6c610
mSimon12 <marcelo12.simon@gmail.com>
2024-10-23 22:46:12 +0200

    feat: add logic to register user and check login

--- flaskr/user.py
+++ flaskr/user.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, request, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flaskr.forms import RegistrationForm, LoginForm
+from flaskr.api_interface import UserInterface
 from flask_login import UserMixin
 
 bp = Blueprint('user', __name__, url_prefix='/user')
@@ -15,7 +17,19 @@ def register_request():
 
     if register_form.validate_on_submit():
         # Add here register procedure
-        return redirect(url_for('user.login_view'))
+        pwd_hash = generate_password_hash(register_form.password.data)
+
+        new_user = {
+            "username": register_form.username.data,
+            "email": register_form.email.data,
+            "password": pwd_hash
+        }
+
+        call_status = UserInterface.add_user(new_user)
+        if not call_status:
+            flash("Not able to add new user!")
+        else:
+            return redirect(url_for('user.login_view'))
 
     return redirect(url_for('user.register_view'))
 
@@ -31,10 +45,20 @@ def login_request():
 
     if login_form.validate_on_submit():
         # Add here login authentication
-        return redirect(url_for('main_page'))
+        user = UserInterface.get_user(username=login_form.username.data)
+        if check_password_hash(user.password, login_form.password.data):
+            return redirect(url_for('main_page'))
+        else:
+            flash("Invalid login data!")
 
     return redirect(url_for('user.login_view'))
 
 
+class User(UserMixin):
 
+    def __init__(self, id, username, email, password):
+        self.id = id
+        self.username = username
+        self.email= email
+        self.password = password
 
