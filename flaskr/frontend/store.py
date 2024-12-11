83c03862a51d2eb8e6f4d5a15cc3830555ea68ec
marcelo.simon <marcelo.simon@gwa-hygiene.de>
2024-12-11 22:06:32 +0100

    feat: frontend uses token stored in browser cookies

--- flaskr/frontend/store.py
+++ flaskr/frontend/store.py
@@ -1,16 +1,19 @@
+import json
 
 from flask import Blueprint, request, render_template, redirect, url_for
 from flask_login import current_user
 from flaskr.frontend.api_interface import ItemsInterface
 from flaskr.frontend.forms import CommentForm, RentItemForm, ReturnItemForm
+import base64
 
 bp = Blueprint('store', __name__, url_prefix='/store')
 comments = []
 
+items_interface = ItemsInterface()
 
 @bp.route('/')
 def store():
-    items = ItemsInterface.get_store_items()
+    items = items_interface.get_store_items()
     items = [(items[item]['id'], items[item]['item'], items[item]['available']) for item in items]
     return render_template("store.html", template_items=items)
 
@@ -21,18 +24,32 @@ def product_view(id):
     rent_form = RentItemForm()
     return_form = ReturnItemForm()
 
+    token = request.cookies.get("access_token_cookie")
+    items_interface.set_token(token)
+
+    if token is not None:
+        payload = token.split(sep='.')[1]
+
+        client_id = int(json.loads(base64.b64decode(payload))['sub'])
+    else:
+        return redirect(url_for('user.login_view'))
+
     if request.method == 'POST':
         if comment_form.validate_on_submit():
             comments.append(comment_form.new_comment.data)
             comment_form.new_comment.data = None
         elif rent_form.validate_on_submit() and rent_form.rent_item.data:
-            ItemsInterface.rent_item(id, current_user.id)
+            call_status, resp_status = items_interface.rent_item(id, client_id)
+            if not call_status:
+                return redirect(url_for('user.login_view'))
         elif return_form.validate_on_submit() and return_form.return_item.data:
-            ItemsInterface.return_item(id, current_user.id)
+            call_status, resp = items_interface.return_item(id, client_id)
+            if not call_status:
+                return redirect(url_for('user.login_view'))
 
         return redirect(url_for('store.product_view', id=id))
 
-    item_info = ItemsInterface.get_item_info(id)
+    item_info = items_interface.get_item_info(id)
     if not item_info:
         return '', 404
     return render_template("product_view.html",
