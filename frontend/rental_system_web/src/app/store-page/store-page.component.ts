e5a10cb41b07a8cf5f2dec895e63a703098dc385
marcelo.simon <marcelo12.simon@gmail.com>
2025-12-23 15:37:00 +0100

    feat: angular front with store page formated

---
+++ frontend/rental_system_web/src/app/store-page/store-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {ItemCardComponent} from '../item-card/item-card.component';
+import { StoreItem } from '../model/store-item';
+
+@Component({
+  selector: 'app-store-page',
+  imports: [
+    ItemCardComponent
+  ],
+  templateUrl: './store-page.component.html',
+  styleUrl: './store-page.component.css'
+})
+export class StorePageComponent {
+  storeItems: StoreItem[] = [
+    { id: 1, name: 'Camera', description: 'DSLR Camera', available: 3 },
+    { id: 2, name: 'Drill', description: 'Electric drill', available: 5 }
+  ];
+
+}
